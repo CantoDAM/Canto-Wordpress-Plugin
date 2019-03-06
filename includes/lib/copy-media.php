5cedfa66f2aa42f764285be3bd9664894a855b2d
Ian Ray <ianthekid@gmail.com>
2019-03-06 00:23:41 -0800

    Add mime_type detection before importing attachment into Wordpress. Notify user on React Modal of the mime_type that is not supported. If allowed, createBlock by mime_type

--- includes/lib/copy-media.php
+++ includes/lib/copy-media.php
@@ -6,7 +6,27 @@
 
 define( 'WP_ADMIN', false );
 define( 'WP_LOAD_IMPORTERS', false );
+$error = null;
 
+/*
+ * Detect Mime type for createBlock type and insertBlocks
+ */
+
+function check_mime_types($haystack, $needle) {
+	$mime_types = [];
+	$found = false;
+	foreach($haystack as $key => $value) {
+		if( strstr($key,"|") ) {
+			$keys = explode("|", $key);
+			foreach($keys as $k) {
+				array_push($mime_types, strtolower($k));
+			}
+		} else {
+			array_push($mime_types, strtolower($key));
+		}
+	}
+	return in_array(strtolower($needle), $mime_types);
+}
 
 function curl_action( $url, $echo ) {
 
@@ -53,8 +73,6 @@ if ( isset( $_POST['post_id'] ) ) {
 
 
 
-
-//if ( isset( $send_id ) ) {
 global $post;
 
 $attachment = $_POST['fbc_id'];
@@ -70,146 +88,131 @@ $response = curl_action( $flight['req'], 0 );
 $response = ( json_decode( $response ) );
 
 
+/* Check if mime_type is allowed */
+$allowed_mime_types = get_allowed_mime_types();
+$uploaded_file_type = pathinfo($response->name);
 
+//$error = $allowed_mime_types;
 
+if( !check_mime_types($allowed_mime_types, $uploaded_file_type['extension']) ) {
+	$error = "This file type is not allowed by your Wordpress theme. Please contact your theme developer to allow support of this file type: '".$uploaded_file_type['extension']."'";
+}
 
-//Get the download url
-$detail = $response->url->download;
-$detail = curl_action( $detail, 1 );
+if($error === null) :
 
-//OLD WAY OF GETTING FLIGHT IMAGE DOWNLOAD
-//list( $httpheader ) = explode( "\r\n\r\n", $detail, 2 );
-//$matches = array();
-//preg_match( '/(Location:|URI:)(.*?)\?x-amz-security-token/', $httpheader, $matches );
-//$location = trim( str_replace( "Location: ", "", $matches[0] ) );
-
+	//Get the download url
+	$detail = $response->url->download;
+	$detail = curl_action( $detail, 1 );
 
 	$matches = array();
 	preg_match( '/(Location:|URI:)(.*?)[\n\r]/i', $detail, $matches );
 	$uri = str_replace( array("Location: ", "location: "), "", $matches[0] );
 	$location = trim( $uri );
 
+	$tmp        = download_url( $location );
+	$file_array = array(
+		'name'     => $response->name,
+		'tmp_name' => $tmp
+	);
 
-$tmp        = download_url( $location );
-$file_array = array(
-	'name'     => $response->name,
-	'tmp_name' => $tmp
-);
+	// Check for download errors
+	if ( is_wp_error( $tmp ) ) {
+		@unlink( $file_array['tmp_name'] );
 
-// Check for download errors
-if ( is_wp_error( $tmp ) ) {
-	@unlink( $file_array['tmp_name'] );
+		$error = $tmp;
+	}
 
-	return $tmp;
-}
-
-$post_data = array(
+	$post_data = array(
 		'post_content' => $_POST['description'],
 		'post_excerpt' => $_POST['caption'],
-);
+	);
+
+	if (! empty($_POST['title'])) {
+		$post_data['post_title'] = $_POST['title'];
+	} else {
+		$post_data['post_title'] = basename($file_array['name']);
+	}
 
 
-if (! empty($_POST['title'])) {
-	$post_data['post_title'] = $_POST['title'];
-} else {
-	$post_data['post_title'] = basename($file_array['name']);
-}
+
+	/**
+	 * Check for Duplicates (existing images imported from Canto)
+	 */
+	$args = array(
+	  'post_type'   => 'attachment',
+	  'post_status' => 'inherit',
+	  'meta_query'  => array(
+	    array(
+	      'key'     => 'fbc_id',
+	      'value'   => $_POST['fbc_id']
+	    )
+	  )
+	);
+	$query = new WP_Query($args);
+	$posts = $query->posts;
+	if($posts && get_option('fbc_duplicates') === "true") {
+		$id = $posts[0]->ID;
+
+		update_post_meta ($id, '_wp_attachment_image_alt' , $_POST['alt']);
+		update_post_meta ($id, 'description' , $_POST['description']);
+		update_post_meta ($id, 'copyright' , $_POST['copyright']);
+		update_post_meta ($id, 'terms' , $_POST['terms']);
+		update_post_meta ($id, 'fbc_id' , $_POST['fbc_id']);
+		update_post_meta ($id, 'fbc_scheme' , $_POST['fbc_scheme']);
 
 
+		$guid = explode("/",$posts[0]->guid);
+		$file = array_pop($guid);
+		$meta = wp_get_attachment_metadata( $id );
+		$uploads = wp_upload_dir();
+		$dir_path = $uploads['path'];
+		$file_sub = $uploads['subdir'];
+		$file_perms = fileperms($dir_path.'/'.$file) & 0777;
 
-/**
- * Check for Duplicates (existing images imported from Canto)
- */
-$args = array(
-  'post_type'   => 'attachment',
-  'post_status' => 'inherit',
-  'meta_query'  => array(
-    array(
-      'key'     => 'fbc_id',
-      'value'   => $_POST['fbc_id']
-    )
-  )
-);
-$query = new WP_Query($args);
-$posts = $query->posts;
-if($posts && get_option('fbc_duplicates') === "true") {
-	$id = $posts[0]->ID;
+		if (file_exists($dir_path.'/'.$file))
+	  	unlink($dir_path.'/'.$file);
 
-	update_post_meta ($id, '_wp_attachment_image_alt' , $_POST['alt']);
-	update_post_meta ($id, 'description' , $_POST['description']);
-	update_post_meta ($id, 'copyright' , $_POST['copyright']);
-	update_post_meta ($id, 'terms' , $_POST['terms']);
-	update_post_meta ($id, 'fbc_id' , $_POST['fbc_id']);
-	update_post_meta ($id, 'fbc_scheme' , $_POST['fbc_scheme']);
-
-
-	$guid = explode("/",$posts[0]->guid);
-	$file = array_pop($guid);
-	$meta = wp_get_attachment_metadata( $id );
-	$uploads = wp_upload_dir();
-	$dir_path = $uploads['path'];
-	$file_sub = $uploads['subdir'];
-	$file_perms = fileperms($dir_path.'/'.$file) & 0777;
-
-	if (file_exists($dir_path.'/'.$file))
-  	unlink($dir_path.'/'.$file);
-
-	if(is_array($meta)) {
-		foreach($meta["sizes"] as $size) {
-			$fileName = $size["file"];
-			// Create array with all old sizes for replacing in posts later
-			$oldfilesAr[] = $thisfile;
-			// Look for files and delete them
-			if (strlen($fileName)) {
-				$fp = $dir_path.'/'.$size["file"];
-				if (file_exists($fp)) {
-					unlink($fp);
+		if(is_array($meta)) {
+			foreach($meta["sizes"] as $size) {
+				$fileName = $size["file"];
+				// Create array with all old sizes for replacing in posts later
+				$oldfilesAr[] = $thisfile;
+				// Look for files and delete them
+				if (strlen($fileName)) {
+					$fp = $dir_path.'/'.$size["file"];
+					if (file_exists($fp)) {
+						unlink($fp);
+					}
 				}
 			}
 		}
+
+		//Copy file for Wordpress media access
+		$copy = copy($tmp,$dir_path.'/'.$file);
+		@chmod($dir_path.'/'.$file, $file_perms);
+
+		// Make thumb and/or update metadata
+		wp_update_attachment_metadata( $id, wp_generate_attachment_metadata( $id, $dir_path.'/'.$file ) );
+		update_attached_file( $id, $dir_path.'/'.$file);
+		if(file_exists($file_array['tmp_name']))
+			unlink($file_array['tmp_name']);
+
+
+	} else {
+		$id = media_handle_sideload( $file_array, $send_id, '', $post_data);
+		if ( is_wp_error( $id ) ) {
+			@unlink( $file_array['tmp_name'] );
+			return $id;
+		}
+		add_post_meta ($id, '_wp_attachment_image_alt' , $_POST['alt']);
+		add_post_meta ($id, 'description' , $_POST['description']);
+		add_post_meta ($id, 'copyright' , $_POST['copyright']);
+		add_post_meta ($id, 'terms' , $_POST['terms']);
+		add_post_meta ($id, 'fbc_id' , $_POST['fbc_id']);
+		add_post_meta ($id, 'fbc_scheme' , $_POST['fbc_scheme']);
 	}
-
-	// Move new file to old location/name
-	//$moveit = move_uploaded_file($tmp, $dir_path.'/'.$file);
-	//$movefile = wp_handle_upload( $uploadedfile, array('test_form' => false) );
-	$copy = copy($tmp,$dir_path.'/'.$file);
-
-	//exit(json_encode(array($file_array['tmp_name'], $dir_path.'/'.basename($file))));
-	//error_get_last()
-	//exit(json_encode($copy));
-
-	@chmod($dir_path.'/'.$file, $file_perms);
-
-	// Make thumb and/or update metadata
-	wp_update_attachment_metadata( $id, wp_generate_attachment_metadata( $id, $dir_path.'/'.$file ) );
-	update_attached_file( $id, $dir_path.'/'.$file);
-	if(file_exists($file_array['tmp_name']))
-		unlink($file_array['tmp_name']);
-
-
-} else {
-	$id = media_handle_sideload( $file_array, $send_id, '', $post_data);
-	if ( is_wp_error( $id ) ) {
-		@unlink( $file_array['tmp_name'] );
-		return $id;
-	}
-	add_post_meta ($id, '_wp_attachment_image_alt' , $_POST['alt']);
-	add_post_meta ($id, 'description' , $_POST['description']);
-	add_post_meta ($id, 'copyright' , $_POST['copyright']);
-	add_post_meta ($id, 'terms' , $_POST['terms']);
-	add_post_meta ($id, 'fbc_id' , $_POST['fbc_id']);
-	add_post_meta ($id, 'fbc_scheme' , $_POST['fbc_scheme']);
-}
-
 
 	$attachment_url = wp_get_attachment_url( $id );
-
-
-	// Additional parameters
-	//$caption = $title = $align = $rel = $size = $alt = '';
-	//$rel     = false;
-	//$html    = get_image_send_to_editor( $id, $caption, $title, $align, $attachment_url, (bool) $rel, $size, $alt );
-
 	$rel = $url = '';
 	$html = $title = isset( $_POST['title'] ) ? $_POST['title'] : '';
 
@@ -257,20 +260,27 @@ if($posts && get_option('fbc_duplicates') === "true") {
 
 	$attachment_id = $id;
 
-	$results = array("attachment" => $html, "attachment_id" => $attachment_id, "attachment_url" => $attachment_url);
+	$mime_string = get_post_mime_type($attachment_id);
+	$mime_type = explode('/', $mime_string);
+
+	$results = array(
+		"attachment" => $html,
+		"attachment_id" => $attachment_id,
+		"attachment_url" => $attachment_url,
+		"attachment_mime" => $mime_type[0]
+	);
 
 
-	if ( isset( $_POST['chromeless'] ) && $_POST['chromeless'] ) {
-		// WP3.5+ media browser is identified by the 'chromeless' parameter
-		//exit( $html );
-		//header('Content-Type: application/json;charset=utf-8');
-		//echo json_encode($results);
+endif; //End Error for $allowed_mime_types
+
+
+if ( isset( $_POST['chromeless'] ) && $_POST['chromeless'] ) {
+	if($error === null) {
 		exit(json_encode($results));
 	} else {
-		//return media_send_to_editor( $html );
-		return media_send_to_editor(json_encode($results));
+		$results = array("error" => $error);
+		exit(json_encode($results));
 	}
-
-
-
-//}
+} else {
+	return media_send_to_editor(json_encode($results));
+}
